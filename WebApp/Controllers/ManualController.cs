9dee0c1a1c4dad3cd92b2852d8755f41a20421e5
Junior Santana <jrlsantana@hotmail.com>
2021-04-16 15:51:08 -0300

    manual

---
+++ WebApp/Controllers/ManualController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    /// <summary>
+    /// Manual do Usuario
+    /// </summary>
+    public class ManualController : Controller
+    {
+        /// <summary>
+        /// Pagina inicial
+        /// </summary>
+        /// <returns></returns>
+        // GET: Manual
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// manual de anomalia
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Anomalias()
+        {
+            return View();
+        }
+        /// <summary>
+        /// Reparos
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Reparos()
+        {
+            return View();
+        }
+        /// <summary>
+        /// Conservas
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Conservas()
+        {
+            return View();
+        }
+        /// <summary>
+        /// Documentos
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Documentos()
+        {
+            return View();
+        }
+    }
+}
